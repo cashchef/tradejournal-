9e77509c4cf073e7d46b24de223a4b61c2f13b35
cashchef <letoyalemein@gmail.com>
2026-05-06 13:43:07 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,1 +1,10 @@
+import Layout from "../components/Layout";
 
+export default function Dashboard() {
+  return (
+    <Layout>
+      <h1>Dashboard</h1>
+      <p>Stats will go here</p>
+    </Layout>
+  );
+}
