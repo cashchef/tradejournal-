efa3f734ff202b9e8f04dcb14771c60e6bd6cda3
cashchef <letoyalemein@gmail.com>
2026-05-06 13:47:28 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,1 +1,12 @@
+import Sidebar from "./Sidebar";
 
+export default function Layout({ children }) {
+  return (
+    <div style={{ display: "flex", height: "100vh", background: "black", color: "white" }}>
+      <Sidebar />
+      <div style={{ flex: 1, padding: "20px" }}>
+        {children}
+      </div>
+    </div>
+  );
+}
